405422f22ebafd3de9ae8ab5c7b7313899236e77
maxrel95 <58341766+maxrel95@users.noreply.github.com>
2022-10-18 17:26:41 +0200

    import data and create initial variables

---
+++ HW1_FE1.R
@@ -0,0 +1,10 @@
+library(data.table) 
+library(readxl)
+# author Maxime Borel
+
+pnl<- read.csv( "PS1_data/callreports_1976_2021wc.csv" ) # read the data
+
+LIQRAT <- pnl$cash +
+SECRAT
+DEPRAT
+COMRAT
