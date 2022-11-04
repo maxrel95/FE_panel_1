83c18e8234f9c08c33c2a25a426cc93be202fb09
maxrel95 <58341766+maxrel95@users.noreply.github.com>
2022-11-04 11:24:22 +0100

    inspecting bhcud difference

--- HW1_FE1.R
+++ HW1_FE1.R
@@ -13,6 +13,25 @@ source("All_functions.R")
 result1 = banksAsLiquidityProvider( dateStart = "1992-03-31", dateEnd = "1996-12-31" ) 
 result2 = banksAsLiquidityProvider( dateStart = "1997-03-31", dateEnd = "2013-12-31" )
 
+pnl <- as.data.frame(fread( "PS1_data/callreports_1976_2021wc.csv" )) # read the data
+
+# indicate to the machine that the vector date is actually date datatype
+pnl$date = strptime(pnl$date, format = "%Y%m%d")
+
+# slice the data and keep only the period under study and the variables of interest
+df9296 = pnl[pnl$date>="1992-03-31" & pnl$date<="1996-12-31", c("date", "rssdid", "bhcid", "chartertype",
+                                                        "fedfundsrepoasset", "securities","assets",
+                                                        "cash", "transdep", "deposits", "commitments",
+                                                        "loans","ciloans", "persloans", "reloans")]
+
+# put some zero where the bhcid is missing
+if (sum(is.na(df9296$bhcid))>0){
+  df9296$bhcid[is.na(df9296$bhcid)] = 0
+}
+
+temp = df9296 %>%
+  group_by(rssdid)%>%
+  filter(length(unique(bhcid))>2)
 
 shiftTable1 = as.data.frame(result2[1]) - as.data.frame(result1[1])
 shiftTable2 = as.data.frame(result2[2]) - as.data.frame(result1[2])
